d745b1fa4197c543bf85e0cf45158f465ccb979e
rabbi-lion <marebrk@icloud.com>
2026-09-01 20:29:13 +0200

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono:pixelsize=30:antialias=true:autohint=true";
+static char *font = "IBM Plex Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
